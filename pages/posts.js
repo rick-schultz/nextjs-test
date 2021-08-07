60131ef1cae5118284e1e5bc7451a9afc4ce95b4
Rick Schultz <rick-schultz@hotmail.com>
2021-08-07 08:41:55 -0300

    create another page

---
+++ pages/posts.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Posts() {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <Contador />
+    </div>
+  );
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  );
+}
+
+export default Posts;
